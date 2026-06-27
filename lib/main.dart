342f8dd995333911d5b014cf7205912f6052fc4c
ujwaljain506-hash <ujwaljain506@gmail.com>
2026-06-27 11:37:49 +0530

    feat: connect Firebase to Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   @override
